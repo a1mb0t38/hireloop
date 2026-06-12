a4d011bf75c2c5e0372ac9671505c98a84cad9de
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-06-12 11:17:34 +0600

    added the loading in recruiter Jobs page

---
+++ src/app/dashboard/recruiter/jobs/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobLoadingPage = () => {
+    return (
+        <div className='flex h-[85vh] items-center justify-center'>
+            <span className="loading loading-bars loading-xl text-green-900"></span>
+        </div>
+    );
+};
+
+export default JobLoadingPage;
